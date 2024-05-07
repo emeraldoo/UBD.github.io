367288f7e26246a754ca10dd0ccceb90a66c3342
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-07 15:03:52 +0200

    Blei war unnötig und nicht gefragt für

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ function Uran238_Cal() {
         alert("Der Jetztige wert des Uran-238 ist negativ, Lösung somit ungültig!")
     }
 }
-function Uran236_Cal() {
+function Uran235_Cal() {
     // E - Erfassen
     var u235_start = document.getElementById("u235_t1").value;
     var u235_end = document.getElementById("u235_t2").value;
@@ -20,17 +20,5 @@ function Uran236_Cal() {
     }
     if (u235_end < 0) {
         alert("Der Jetztige wert des Uran-235 ist negativ, Lösung somit ungültig!")
-    }
-}
-function Blei_Cal() {
-    // E - Erfassen
-    var blei_start = document.getElementById("blei_t1").value;
-    var blei_end = document.getElementById("blei_t2").value;
-    //V - Verarbeiten
-    if  (blei_start < 0) {
-        alert("Der Blei-Urpsprungswert ist negativ, Lösung somit ungültig!")
-    }
-    if (blei_end < 0) {
-        alert("Der Jetztige Blei-Wert ist negativ, Lösung somit ungültig!")
     }
 }
