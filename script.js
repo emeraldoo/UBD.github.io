0673ba23c36424e626c1b7a4594965fee16a334c
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-07 13:26:57 +0200

    Blei und Uranium als Variable im JS erhältlich

---
+++ script.js
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+function Calculate() {
+    var u238 = document.getElementById("uran238").value;
+    var u235 = document.getElementById("uran235").value;
+    var b206 = document.getElementById("blei206").value;
+    var b207 = document.getElementById("blei207").value;
+}
