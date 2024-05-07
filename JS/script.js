ede7a9c05fb8c7b830e4d57ce4dd375175c02a65
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-07 14:28:47 +0200

    Sections Clean-up, headings gefixt

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,3 @@
-function Calculate() {
-    var u238 = document.getElementById("uran238").value;
-    var u235 = document.getElementById("uran235").value;
-    var b206 = document.getElementById("blei206").value;
-    var b207 = document.getElementById("blei207").value;
+function Uran238_Cal() {
+    
 }
