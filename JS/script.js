79a341a6fff2a3aac6f8781533ed115b58b1e6ff
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-07 14:38:25 +0200

    E - Erfassen der benötitgten variabeln in JS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
 function Uran238_Cal() {
-    
+    // E - Erfassen
+    var u238_start = document.getElementById("u238_t1").value;
+    var u238_end = document.getElementById("u238_t2").value;
+
+}
+function Uran236_Cal() {
+        // E - Erfassen
+    var u235_start = document.getElementById("u235_t1").value;
+    var u235_end = document.getElementById("u235_t2").value;
+}
+function Blei_Cal() {
+        // E - Erfassen
+    var blei_start = document.getElementById("blei_t1").value;
+    var blei_end = document.getElementById("blei_t2").value;
 }
