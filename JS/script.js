197ebdc874274d1908e210cdcbfd8c70d943b5c9
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-07 13:49:58 +0200

    Ordnerstruktur Erstellt

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,6 @@
 function Calculate() {
-    // E - Ermitteln:
     var u238 = document.getElementById("uran238").value;
     var u235 = document.getElementById("uran235").value;
     var b206 = document.getElementById("blei206").value;
     var b207 = document.getElementById("blei207").value;
-    // V - Verarbeiten:
-    
 }
