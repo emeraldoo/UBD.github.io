4ea7508227cea1f28afe4cdc1516a560ea5086de
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-08 09:31:33 +0200

    Number() zu den var Hinzugefügt

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,10 @@ function Uran238_Cal() {
     var u238_start = document.getElementById("u238_t1").value;
     var u238_end = document.getElementById("u238_t2").value;
     //V - Verarbeiten
-    if  (u238_start < 0) {
+    if  (Number(u238_start) < 0) {
         alert("Der Ursprungswert des Uran-238 ist negativ, Lösung somit ungültig!")
     }
-    if (u238_end < 0) {
+    if (Number(u238_end) < 0) {
         alert("Der Jetztige wert des Uran-238 ist negativ, Lösung somit ungültig!")
     }
 }
@@ -15,10 +15,10 @@ function Uran235_Cal() {
     var u235_start = document.getElementById("u235_t1").value;
     var u235_end = document.getElementById("u235_t2").value;
     //V - Verarbeiten
-    if  (u235_start < 0) {
+    if  (Number(u235_start) < 0) {
         alert("Der Ursprungswert des Uran-235 ist negativ, Lösung somit ungültig!")
     }
-    if (u235_end < 0) {
+    if (Number(u235_end) < 0) {
         alert("Der Jetztige wert des Uran-235 ist negativ, Lösung somit ungültig!")
     }
 }
