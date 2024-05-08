24027f7bd7c1e7e029183430cbb1f04a4b00e31e
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-08 10:27:31 +0200

    I forgor "Number()" zu den variabeln hinzuzufügen

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,7 @@ function Uran238_Cal() {
       );
     }
     // formel: return (-1 / 4'468'000'000) * Math.log(u238_t2 / u238_t1);
-    var solution = (-1 / 4468000000) * Math.log(u238_end / u238_start);
+    var solution = (-1 / 4468000000) * Math.log(umber(u238_end) / Number(u238_start));
     alert (solution);
     document.getElementById("u238_sol").innerHTML = solution;
   }
@@ -34,7 +34,7 @@ function Uran238_Cal() {
         "Der Jetztige wert des Uran-235 ist negativ, Lösung somit ungültig!"
       );
     }
-    var solution = (-1 / 703800000) * Math.log(u238_start / u238_end);
+    var solution = (-1 / 703800000) * Math.log(Number(u238_start) / Number(u238_end));
     alert(solution);
     document.getElementById("u238_sol").innerHTML = solution;
   }
