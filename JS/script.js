418cf2f5f6199e5ba01ce183dcf01d69c1c973f8
LeonZingg <167877324+LeonZingg@users.noreply.github.com>
2024-05-22 15:27:51 +0200

    Script umgearbeitet - HOFFENTLICH funktionierts

--- JS/script.js
+++ JS/script.js
@@ -4,15 +4,21 @@ function Uran238_Cal() {
   var u238_end = document.getElementById("uran238_t2").value;
   //V - Verarbeiten
   if (Number(u238_start) < 0) {
-    alert("Der Ursprungswert des Uran-238 ist negativ, Lösung somit ungültig!");
+    alert(
+      "Der Ursprungswert des Uran-238 ist negativ, Lösung somit ungültig!"
+    );
     return;
   }
   if (Number(u238_end) < 0) {
-    alert("Der jetzige Wert des Uran-238 ist negativ, Lösung somit ungültig!");
+    alert(
+      "Der jetzige Wert des Uran-238 ist negativ, Lösung somit ungültig!"
+    );
     return;
   }
   // Formel: return (-1 / 0.000000000155125) * Math.log(u238_t2 / u238_t1);
-  var solution = (-1 / 0.000000000155125) * Math.log(Number(u238_end) / Number(u238_start));
+  var solution =
+    (-1 / 0.000000000155125) *
+    Math.log(Number(u238_end) / Number(u238_start));
   solution = Formattieren(solution);
   document.getElementById("u238_sol").innerHTML = solution;
 }
@@ -23,14 +29,20 @@ function Uran235_Cal() {
   var u235_end = document.getElementById("u235_t2").value;
   //V - Verarbeiten
   if (Number(u235_start) < 0) {
-    alert("Der Ursprungswert des Uran-235 ist negativ, Lösung somit ungültig!");
+    alert(
+      "Der Ursprungswert des Uran-235 ist negativ, Lösung somit ungültig!"
+    );
     return;
   }
   if (Number(u235_end) < 0) {
-    alert("Der jetzige Wert des Uran-235 ist negativ, Lösung somit ungültig!");
+    alert(
+      "Der jetzige Wert des Uran-235 ist negativ, Lösung somit ungültig!"
+    );
     return;
   }
-  var solution = (-1 / 0.00000000098485) * Math.log(Number(u235_end) / Number(u235_start));
+  var solution =
+    (-1 / 0.00000000098485) *
+    Math.log(Number(u235_end) / Number(u235_start));
   solution = Formattieren(solution);
   document.getElementById("u235_sol").innerHTML = solution;
 }
@@ -62,17 +74,15 @@ function Uran238Decay_Cal() {
 function Formattieren(solution) {
   solution = Math.ceil(solution / 100000) * 100000;
   if (solution >= 1000000000000) {
-      solution = (solution / 1000000000000).toFixed(6) + " Bil";
-      return solution;  
-  }
-  else if (solution >= 1000000000) {
-      solution = (solution / 1000000000).toFixed(3) + " Mrd";
-      return solution;
-  } 
-  else if (solution >= 1000000) {
-      solution = (solution / 1000000).toFixed(1) + " Mio.";
-      return solution;
+    solution = (solution / 1000000000000).toFixed(6) + " Bil";
+    return solution;
+  } else if (solution >= 1000000000) {
+    solution = (solution / 1000000000).toFixed(3) + " Mrd";
+    return solution;
+  } else if (solution >= 1000000) {
+    solution = (solution / 1000000).toFixed(1) + " Mio.";
+    return solution;
   } else {
-      return solution; // Falls die Zahl kleiner als 1 Mio. ist
+    return solution; // Falls die Zahl kleiner als 1 Mio. ist
   }
 }
